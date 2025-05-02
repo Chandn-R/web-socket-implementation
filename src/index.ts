4fd9d64a76ec27389c4785aa89d8289fb0a5d5b0
Chandan <chandan@Chandans-MacBook-Air.local>
2025-05-03 01:23:09 +0530

    improvement

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,23 @@ import express from 'express'
 import { WebSocketServer } from 'ws'
 
 const app = express()
-const httpServer = app.listen(8080)
+const httpServer = app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+
+})
 
 const wss = new WebSocketServer({ server: httpServer });
 
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+    ws.on('error', console.error);
 
-  ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
     });
-  });
 
-  ws.send('Hello! Message From Server!!');
+    ws.send('Hello! Message From Server!!');
 });
